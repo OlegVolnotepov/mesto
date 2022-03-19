e93484dc47bb9754faf023adca77628f92327285
ovolnotepov <ovolnotepov@ocs.ru>
2022-03-19 16:20:35 +0300

    feat: Do img popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,51 @@
 const popup = document.querySelector('.popup');
+const popupCard = document.querySelector('.popup-card');
 const editbutton = document.querySelector('.profile__edit');
 const closeButton = document.querySelector('.popup__close');
 const popupName = document.getElementById('name');
 const popupAbout = document.getElementById('about');
 const name = document.querySelector('.profile__name');
 const about = document.querySelector('.profile__about');
+const elementsCard = document.querySelector('#elements__card').content; //карточка места
+const addCard = document.querySelector('.elements');
+const cardAddButton = document.querySelector('.profile__add-button');
+const cardAddCloseButton = document.querySelector('.popup-card__close');
+const cardSubmitButton = document.querySelector('.popup-card__button');
+const cardTitle = document.getElementById('card-title');
+const cardUrl = document.getElementById('card-url');
 
-function showPopup() {
-  popupName.setAttribute('value', name.textContent);
-  popupAbout.setAttribute('value', about.textContent);
-  popup.classList.toggle('popup_opened');
-}
 
-editbutton.addEventListener('click', showPopup); 
-closeButton.addEventListener('click', showPopup); 
+//Открытие и закрытие попапа редактирования информации - событие
+editbutton.addEventListener('click', showPopup);
+closeButton.addEventListener('click', showPopup);
 
-//закрываем попап при клике за пределами
+//Открытие и закрытие попапа добавления карточки - событие
+cardAddButton.addEventListener('click', showPopupCard);
+cardAddCloseButton.addEventListener('click', showPopupCard);
+
+//Закрытие попапа при клике за пределами - событие - сделать такую же для добавления карточки!!!
 popup.addEventListener('click', function(event) {
   if(event.target === event.currentTarget) {
     popup.classList.toggle('popup_opened');
   }
 })
 
+//Функция открытия попапа добавления информации
+function showPopup() {
+  popupName.setAttribute('value', name.textContent);
+  popupAbout.setAttribute('value', about.textContent);
+  popup.classList.toggle('popup_opened');
+}
+
+//Функция открытия попапа добавления карточки
+function showPopupCard() {
+  popupCard.classList.toggle('popup-card_opened');
+}
+
+//Функция обработки формы редактирования
 function formSubmitHandler (evt) {
-  evt.preventDefault(); 
-  // Получите значение полей Input и из свойства value
+  evt.preventDefault();
+  // Получите значение полей Input из свойства value
   let inputName = popupName.value
   let inputAbout = popupAbout.value
   //вставляем на страницу новые значения
@@ -33,4 +54,101 @@ function formSubmitHandler (evt) {
   popup.classList.toggle('popup_opened');
 }
 
-popup.addEventListener('submit', formSubmitHandler); 
+//Кнопка отправки новой информации - событие
+popup.addEventListener('submit', formSubmitHandler);
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+//Функция лайка
+function like(evt) {
+  evt.target.classList.toggle('elements__like_active');
+}
+
+//Функция удаления
+function delCard(evt) {
+  const cardForRemove = evt.currentTarget.closest('.elements__card')
+  cardForRemove.remove();
+}
+
+//Рендер карточек на странице
+// initialCards.forEach((i) => {
+//   let card = elementsCard.querySelector('.elements__card').cloneNode(true); // клонируем содержимое тега template
+//   card.querySelector('.elements__img').src = i.link; // наполняем содержимым
+//   card.querySelector('.elements__title').textContent = i.name; // наполняем содержимым
+//   addCard.append(card); // отображаем на странице
+//   like()
+// })
+
+//Функция добавления новой карточки на страницу
+function renderNewCard(evt) {
+  evt.preventDefault();
+  // Получите значение полей Input из свойства value
+  const inputUrl = cardUrl.value;
+  const inputTitle = cardTitle.value;
+  const card = elementsCard.querySelector('.elements__card').cloneNode(true); // клонируем содержимое тега template
+  card.querySelector('.elements__img').src = inputUrl;
+  card.querySelector('.elements__title').textContent = inputTitle;
+  const likeButton = card.querySelector('.elements__like');
+  likeButton.addEventListener('click', like);
+
+  //Удаление карточки
+  const delButton = card.querySelector('.elements__trash');
+  delButton.addEventListener('click', delCard);
+
+  addCard.prepend(card);
+  showPopupCard();
+}
+
+//Отправка карточки на страницу - событие
+popupCard.addEventListener('submit', renderNewCard);
+
+
+
+//тестирую добавление карточек из массива через функцию
+
+function renderCard(link, name) {
+  let card = elementsCard.querySelector('.elements__card').cloneNode(true); // клонируем содержимое тега template
+  card.querySelector('.elements__img').src = link; // наполняем содержимым
+  card.querySelector('.elements__title').textContent = name; // наполняем содержимым
+  const likeButton = card.querySelector('.elements__like');
+  likeButton.addEventListener('click', like);
+
+  //Удаление карточки
+  const delButton = card.querySelector('.elements__trash');
+  delButton.addEventListener('click', delCard);
+
+  addCard.append(card); // отображаем на странице
+ }
+
+initialCards.forEach((item) => {
+  renderCard(item.link, item.name)
+})
+
+
+//Открытие попапа
